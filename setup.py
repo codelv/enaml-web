a8ef144ed94751abb6364277d2f47201840b05c8
frmdstryr <frmdstryr@gmail.com>
2017-04-17 16:21:26 -0400

    Add setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+'''
+Created on Apr 17, 2017
+
+@author: jrm
+'''
+from setuptools import setup, find_packages
+
+setup(
+    name='enaml-web',
+    version='0.1.0',
+    author='frmdstryr',
+    author_email='frmdstryr@gmail.com',
+    url='https://gitlab.com/frmdstryr/enaml-web',
+    description='Web component toolkit for Enaml',
+    license = "MIT",
+    long_description=open('README.md').read(),
+    requires=['enaml',],
+    install_requires=['distribute', 'enaml >= 0.9.8'],
+    packages=find_packages(),
+)
