f653d36709302606497c202ca528a6f7ac80a1b4
frmdstryr <frmdstryr@gmail.com>
2017-12-06 10:03:35 -0500

    Some pep 8 fixes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='enaml-web',
-    version='0.1.0',
+    version='0.1.1',
     author='frmdstryr',
     author_email='frmdstryr@gmail.com',
     url='https://gitlab.com/frmdstryr/enaml-web',
--- web/core/block.py
+++ web/core/block.py
@@ -1,4 +1,4 @@
-'''
+"""
 Copyright (c) 2017, Jairus Martin.
 
 Distributed under the terms of the MIT License.
@@ -8,13 +8,14 @@ The full license is in the file COPYING.txt, distributed with this software.
 Created on Apr 15, 2017
 
 @author: jrm
-'''
+"""
 from atom.api import ForwardInstance, Enum
 from enaml.core.declarative import Declarative, d_
 
 
 class Block(Declarative):
-    """ An object which dynamically insert's its children into another block's parent object.
+    """ An object which dynamically insert's its children into another block's 
+    parent object.
     
     The 'Block' object is used to cleanly and easily insert it's children
     into the children of another object. The 'Object' instance assigned to the
@@ -28,7 +29,7 @@ class Block(Declarative):
     block = d_(ForwardInstance(lambda: Block))
     
     #: If replace, replace all parent's children (except the block of course) 
-    mode = d_(Enum('replace','append'))
+    mode = d_(Enum('replace', 'append'))
     
     def initialize(self):
         """ A reimplemented initializer.
@@ -39,19 +40,18 @@ class Block(Declarative):
         """
         super(Block, self).initialize()
         
-        if self.block: #: This block is setting the content of another block
+        if self.block:  #: This block is setting the content of another block
             #: Remove the existing blocks children
-            if self.mode=='replace':
+            if self.mode == 'replace':
                 #: Clear the blocks children
                 for c in self.block.children:
                     c.destroy()
             #: Add this blocks children to the other block
             self.block.insert_children(None, self.children)
             
-        else: #: This block is inserting it's children into it's parent
-            self.parent.insert_children(self,self.children)
+        else:  #: This block is inserting it's children into it's parent
+            self.parent.insert_children(self, self.children)
         
-
     def _observe_block(self, change):
         """ A change handler for the 'objects' list of the Include.
 
@@ -62,9 +62,8 @@ class Block(Declarative):
         """
         if self.is_initialized:
             if change['type'] == 'update':
-                raise NotImplementedError
                 old_block = change['oldvalue']
-                old_block.parent.remove_children(old_block,self.children)
+                old_block.parent.remove_children(old_block, self.children)
                 new_block = change['value']
                 new_block.parent.insert_children(new_block, self.children)
                 
@@ -73,19 +72,17 @@ class Block(Declarative):
             return
         if change['type'] == 'update':
             if self.block:
-                if self.mode=='replace':
+                if self.mode == 'replace':
                     self.block.children = change['value']
                 else:
                     for c in change['oldvalue']:
                         self.block.children.remove(c)
                         c.destroy()
-                    before = self.block.children[-1] if self.block.children else None
-                    self.block.insert_children(before,change['value'])
+                    before = (self.block.children[-1]
+                              if self.block.children else None)
+                    self.block.insert_children(before, change['value'])
             else:
                 for c in change['oldvalue']:
                     if c not in change['value']:
                         c.destroy()
-                self.parent.insert_children(self,change['value']) 
-        
-    
-    
+                self.parent.insert_children(self, change['value'])
