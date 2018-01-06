982a458cb230df13afbf481cec06291e8251eab4
frmdstryr <frmdstryr@gmail.com>
2018-01-06 14:36:48 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='enaml-web',
-    version='0.1.2',
+    version='0.2.0',
     author='frmdstryr',
     author_email='frmdstryr@gmail.com',
     url='https://gitlab.com/frmdstryr/enaml-web',
