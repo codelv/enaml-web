f6a6064de1eb18e053d7f4983d762ada02115cd8
frmdstryr <frmdstryr@protonmail.com>
2019-03-07 20:17:37 -0500

    Fix nb on 2.7

--- web/impl/lxml_ipynb.py
+++ web/impl/lxml_ipynb.py
@@ -18,18 +18,18 @@ from web.components.ipynb import ProxyNotebook
 
 class NotebookComponent(RawComponent, ProxyNotebook):
     """ A component for rendering Jupyter Notebooks. """
-    
+
     #: Exporter
     exporter = Instance(HTMLExporter, ())
-    
+
     def set_source(self, source):
         # Parse md and put in a root node
         source, resources = self.exporter.from_notebook_node(
             nbformat.reads(source, as_version=self.declaration.version))
         # Parse source to html
         super(NotebookComponent, self).set_source(
-            "<div>{}</div>".format(source))
-        
+            u"<div>{}</div>".format(source))
+
     def set_version(self, version):
         self.set_source(self.declaration.source)
 
