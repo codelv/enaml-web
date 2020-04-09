9c98e09210c4f16b23a9bf9d7eeaa236e3308a5f
frmdstryr <frmdstryr@protonmail.com>
2020-04-09 09:43:57 -0400

    Delete some outdated examples

--- examples/simple_site/__init__.py
+++
@@ -1,5 +0,0 @@
-from .models import Site
-
-def load(app):
-    """ Load the site """
-    return Site(app=app)
--- examples/simple_site/static/materialize/js/init.js
+++
@@ -1,8 +0,0 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-    $('select').material_select();
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
